a0f03fda4a638b9c1730c30f986dbb146c936fae
JungSuHwan23 <short9666@gmail.com>
2023-07-16 17:51:21 +0900

    PY input-2

--- input.py
+++ input.py
@@ -38,3 +38,15 @@ print(s, s, s)
 #기호를 중심으로 구분하여 입출력
 a, b = input().split(':') #':'를 기준으로 자름
 print(a, b, sep=':') #':'을 사이에 주고 출력
+
+
+#2023-07-16
+
+#Y.M.D -> D-M-Y (code up 6019)
+y, m, d = input().split('.') #'.'을 기준으로 잘라 순서대로 저장
+print(d, m, y, sep='-') #'-'를 사이에 두고 출력
+
+#주민번호입력 (code up 6020)
+n, m = input().split('-')
+print(n, m, sep='') #(sep = '')을 사용하면 n과 m 사이에 공백없이 출력할 수 있다
+
